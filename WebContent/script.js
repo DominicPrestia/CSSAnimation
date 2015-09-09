40123017a3027ff3b87f54c9a2d08fda532f3df4
Dominic Prestia <dominicprestia0@gmail.com>
2015-09-09 02:45:38 -0500

    9-9-15
    JavaScript Update

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,15 +1,42 @@
 var clicked = 0;
-	
 	//click function to animate all objects
 	function danceFunction(){
-		
 		if(clicked == 0){
-				$(".box").addClass("rotate");
+			var animation = ["scale", "rotate"];
+			var count = animation.length;
+			var finalAnim;
+			
+			$(".box").each(function(index){	
+				var randomVar = Math.random()*2;
+				var selectedNum = Math.floor((randomVar));
+					finalAnim =animation[selectedNum];	
+				$(this).addClass(finalAnim);
+			});
+		
+			setTimeout(function(){
+				$(".box").each(function(index){
+					for(i = 0; i < animation.length; i++){
+						if($(this).hasClass(animation[i])){
+							finalAnim = animation[i];
+							$(this).removeClass(finalAnim);
+							i = animation.length;
+						}
+					}
+				
+				});
+				
+				clicked = 0;
+				
+			}, 2000);
+			
+			
+			clicked = 1;
+			
+	
+	
+			
+			
+				//$(".box").addClass("scale");
 				//flag variable so user can't spam click which potentially disjoints the animation
-				clicked = 1;
-				setTimeout(function(){
-					$(".box").removeClass("rotate");
-					clicked = 0;
-				}, 2000);	
 			}
 	}
